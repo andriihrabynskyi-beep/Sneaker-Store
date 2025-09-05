3274cbcc4dcc8f04f205a1000c43ec03fc280b4f
Andrii Hrabynskyi <andrii.hrabinsky@gmail.com.com>
2025-09-05 22:21:53 +0200

    Update page.tsx and _app.tsx with GTM and fixes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,25 @@
+// src/pages/_document.tsx
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Google Tag Manager - Head */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id=GTM-KRD7X9Z2'+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KRD7X9Z2');
+            `,
+          }}
+        />
+      </Head>
       <body>
-        {/* GTM noscript */}
+        {/* Google Tag Manager - Body */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-KRD7X9Z2"
@@ -14,7 +28,6 @@ export default function Document() {
             style={{ display: "none", visibility: "hidden" }}
           ></iframe>
         </noscript>
-
         <Main />
         <NextScript />
       </body>
